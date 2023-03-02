81f88a758eae7afe8caa848d3350cadb89f057a9
lijovijayan <lijovijayan00@gmail.com>
2023-03-02 23:34:38 +0530

    typo fix

--- main.py
+++ main.py
@@ -29,17 +29,17 @@ def plot_cost(cost_list):
     plt.show()
 
 
-def f_x(X, W, b):
+def predict(X, W, b):
     fx = np.dot(X, W) + b
     return fx
 
 
-def cost_fn(X, y, W, b):
+def compute_cost(X, y, W, b):
     m = X.shape[0]
 
     total_cost = 0
     for xi, yi in zip(X, y):
-        total_cost += np.square(f_x(xi, W, b) - yi)
+        total_cost += np.square(predict(xi, W, b) - yi)
 
     cost = (1 / (2 * m)) * total_cost
     return cost
@@ -50,7 +50,7 @@ def dj_dw(X, y, W, b):
     djdw = np.zeros(X.shape[1])
     m = X.shape[0]
     for xi, yi in zip(X, y):
-        djdw += np.multiply(xi, (f_x(xi, W, b) - yi))
+        djdw += np.multiply(xi, (predict(xi, W, b) - yi))
     return (1 / m) * djdw
 
 
@@ -59,11 +59,11 @@ def dj_db(X, y, W, b):
     djdb = 0
     m = X.shape[0]
     for xi, yi in zip(X, y):
-        djdb += (f_x(xi, W, b) - yi)
+        djdb += (predict(xi, W, b) - yi)
     return (1 / m) * djdb
 
 
-def descent(X, y, W, b, alpha):
+def compute_gradient(X, y, W, b, alpha):
     temp_w = W - (alpha * dj_dw(X, y, W, b))
     temp_b = b - (alpha * dj_db(X, y, W, b))
     return temp_w, temp_b
@@ -73,8 +73,8 @@ def gradient_descent(X, y, n, W, b, alpha):
     cost_list = np.array([])
 
     for i in range(0, n):
-        W, b = descent(X, y, W, b, alpha)
-        cost = cost_fn(X, y, W, b)
+        W, b = compute_gradient(X, y, W, b, alpha)
+        cost = compute_cost(X, y, W, b)
         cost_list = np.append(cost_list, cost)
 
     # print(cost_list)
@@ -88,14 +88,15 @@ def init():
     W = np.zeros(X.shape[1])
     b = 0
     alpha = 0.01
-    W, b = gradient_descent(X, y, 500, W, b, alpha)
+    num_iters = 500
+    W, b = gradient_descent(X, y, num_iters, W, b, alpha)
     print(f"W: {W}")
     print(f"B: {b}")
 
     test_data_index = 1
     test_data = X[test_data_index]
     expected_output = y[test_data_index]
-    predicted_output = f_x(test_data, W, b)
+    predicted_output = predict(test_data, W, b)
 
     print(f"expected output: {expected_output} \n predicted output: {predicted_output}")
 
